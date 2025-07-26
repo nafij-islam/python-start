c1f00b505d149955c113d3c435774d65348b16f7
nafij-islam <sahariannafis70@gmail.com>
2025-07-26 19:03:02 +0600

    update

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 print("Hello python")
-x=10
+x=40
 y=30
 
 sum=x+y
 
 print(sum)
+
+if y > x:
+    print("hello world")
+
+else: print("hello javascrip")
+
+
+list=["apple", "banana", "mango"]
+
+print(list[3:])
+
+if "apple" in list:
+    print('yes apple is in the list')
