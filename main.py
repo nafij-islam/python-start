b1422c2f1e44aefdb54e1e0c7c9a320b9a032ba1
nafij-islam <sahariannafis70@gmail.com>
2025-07-26 19:17:13 +0600

    update

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-print("Hello python")
-x=40
-y=30
+# print("Hello python")
+# x=40
+# y=30
 
-sum=x+y
+# sum=x+y
 
-print(sum)
+# print(sum)
 
-if y > x:
-    print("hello world")
+# if y > x:
+#     print("hello world")
 
-else: print("hello javascrip")
+# else: print("hello javascrip")
 
 
-list=["apple", "banana", "mango"]
+# list=["apple", "banana", "mango"]
 
-print(list[3:])
+# print(list[3:])
 
-if "apple" in list:
-    print('yes apple is in the list')
+# if "apple" in list:
+#     print('yes apple is in the list')
+
+
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/show-text', methods=['POST'])
+def show_text():
+    return "<h2>Hello from Python (Flask)!</h2>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
